31377547734248d0c3f1f5b7754c25c5f22e75c3
Rahul <rahulagc1996@gmail.com>
2026-08-22 22:03:55 +0530

    latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import api from "./api/axios";
 
+interface WeatherData {
+  [key: string]: unknown;
+}
+
 function App() {
+  const [data, setData] = useState<WeatherData[]>([]);
   useEffect(() => {
     const getWeather = async () => {
       const response = await api.get("/WeatherForecast");
+      setData(response.data);
       console.log(response.data);
 
       const apiUrl = import.meta.env.VITE_API_URL;
@@ -20,6 +26,7 @@ function App() {
   return (
     <>
       <h1>React App</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
